70ee63806d857aac5fb83409337933c720abfee4
Joe Cox <joe@factual.com>
2017-08-03 15:02:57 -0700

    Update maril.user.js

--- userscript/maril.user.js
+++ userscript/maril.user.js
@@ -4,7 +4,7 @@
 // @version      0.1.0
 // @description  Marathon Application Run In Local
 // @author       agate<agate.hao@gmail.com>
-// @match        http://PUT.YOUR.MARATHON.DOMAIN.HERE
+// @match        http://PUT.YOUR.MARATHON.DOMAIN.HERE/*
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setClipboard
 // ==/UserScript==
